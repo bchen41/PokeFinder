daf36e299d95f925e1a97f7b92064b690ed8ff57
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2021-11-16 14:04:52 -0500

    changes none

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,9 @@ fetch("https://api.pokemontcg.io/v2/cards/", {
   .then(function (cards) {
     console.log(cards);
     console.log(cards.count);
-    console.log(cards.data[1]);
-    console.log(cards.data[1].id);
-    console.log(cards.data[1].images.small);
+    console.log(cards.data[12]);
+    console.log(cards.data[12].id);
+    console.log(cards.data[12].images.small);
 
     for (var i = 0; i < cards.data.length; i++) {
       // creat image elements
